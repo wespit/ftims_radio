ae03439aa80601b687f4dbc41bffa6b49049f2b2
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2010-04-08 17:00:25 +0200

    Initialized repository

---
+++ src/MainController/Main.java
@@ -0,0 +1,13 @@
+package MainController;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
